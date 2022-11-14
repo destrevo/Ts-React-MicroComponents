2e50343a07f3713c7543b8d412e2474a6bf07d27
destrevo <maczko.andor@gmail.com>
2022-11-14 23:09:28 +0100

    fancy incrementing component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import {CollapseData} from './CollapseComponent/CollapseData';
 import Collapse from './CollapseComponent/Collapse';
 import ProgressLine from './ProgressLineComponent/ProgressLine';
+import IncrementCounter from './IncrementingCounterComponent/IncrementCounter';
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <ProgressLine options={[1, 2, 3, 4, 5, 6, 7]}/>
       <div className='line'/>
       <Collapse data={CollapseData} />
+      <div className='line'/>
+      <IncrementCounter start={0} end={1000} duration={5000} uniqeuId={'unique-div-id'} />
       <div className='line'/>
     </div>
   );
---
+++ src/IncrementingCounterComponent/IncrementCounter.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react'
+import "./IncrementCounter.css"
+
+interface IncrementCounterProps {
+    start: number
+    end: number
+    duration: number
+    uniqeuId: string
+}
+
+export default function IncrementCounter({ start, end, duration, uniqeuId }: IncrementCounterProps): JSX.Element {
+    const [restart, setRestart] = React.useState(false)
+
+    const incrementingCounter = (start: number, end: number, duration: number, callback: any) => {
+        let range = end - start;
+        let minTimer = 50;
+        let stepTime = Math.abs(Math.floor(duration / range));
+        stepTime = Math.max(stepTime, minTimer);
+        let startTime = new Date().getTime();
+        let endTime = startTime + duration;
+        let timer: any;
+        function run() {
+            let now = new Date().getTime();
+            let remaining = Math.max((endTime - now) / duration, 0);
+            let value = Math.round(end - (remaining * range));
+            callback(value);
+            if (value === end) {
+                clearInterval(timer);
+            }
+        }
+        timer = setInterval(run, stepTime);
+        run();
+    }
+
+    useEffect(() => {
+        //run after the html is rendered
+        incrementingCounter(start, end, duration, (value: any) => {
+            document.getElementById(uniqeuId)!.innerHTML = value;
+        })
+    }, [restart])
+
+
+    return (
+        <>
+            <div id={uniqeuId} className="counter" />
+            {/* remove the button in real cenario */}
+            <button onClick={()=>{setRestart(x=>!x)}}>restart to test</button>
+        </>
+    )
+}
